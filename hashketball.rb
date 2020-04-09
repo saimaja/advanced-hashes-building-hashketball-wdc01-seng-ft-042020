9d95f8015d6f9b32ce670b646a07aeb2d5b95abf
Saima Akhtar <saimaakhtar@gmail.com>
2020-04-09 18:53:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,7 +2,15 @@
 game_hash = {
   :home => {:team_name =>
             :colors =>
-            :players => [{
+            :players => [{:player_name
+                          :number
+                          :shoe
+                          :points
+                          :rebounds
+                          :assists
+                          :steals
+                          :blocks
+                          :slam_dunks
               ]
   },
   :away => {:team_name =>
