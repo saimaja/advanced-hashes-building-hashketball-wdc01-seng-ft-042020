e34df9066f1697fa1c120232d7e917b362be52b7
Saima Akhtar <saimaakhtar@gmail.com>
2020-04-09 18:58:35 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,22 +2,22 @@
 game_hash = {
   :home => {:team_name => "Brooklyn Nets",
             :colors => "Black", "White",
-            :players => [{:player_name
-                          :number
-                          :shoe
-                          :points
-                          :rebounds
-                          :assists
-                          :steals
-                          :blocks
-                          :slam_dunks
+            :players => [{:player_name => "Alan Anderson", "Reggie Evans",
+                          :number => "0", "30",
+                          :shoe => "16", "14",
+                          :points => "22", "12",
+                          :rebounds => "12", "12",
+                          :assists => "12", "12",
+                          :steals => "3", "12",
+                          :blocks => "1", "12",
+                          :slam_dunks => "1", "7",
             }
               ]
   },
-  :away => {:team_name =>
-            :colors =>
-            :players => {:player_name
-                          :number
+  :away => {:team_name => "Charlotte Hornets",
+            :colors => "Turquoise", "Purple",
+            :players => {:player_name => "Jeff Adrien", "Bismack Biyombo",
+                          :number => "4", "0",
                           :shoe
                           :points
                           :rebounds
